19867e9b5fad1cf64f7c70ad0b756712731b7f53
Bruno Luiz de Deus Adão <brunoblda@gmail.com>
2024-05-24 18:19:00 -0300

    :recycle: refactor: using abstract identification column higienization

--- src/logical_layer/data_manipulation.py
+++ src/logical_layer/data_manipulation.py
@@ -1,24 +1,26 @@
 """Data manipulation class"""
 
+from abc import ABC, abstractmethod
+
 import pandas as pd
 
 
-class DataManipulation:
+class DataManipulation(ABC):
     """Data manipulation class"""
 
     # init method
-    def __init__(self, dt_table: pd.DataFrame) -> None:
-        self.dt_table = dt_table
+    def __init__(self) -> None:
         self.column_email_name = "E-MAIL"
-        self.column_identification_name = None
 
     # get column list of identification values
+    @abstractmethod
     def get_identification_column_list(self) -> pd.DataFrame:
         """Get column list"""
 
         raise NotImplementedError("Subclass must implement this method")
 
     # set the value in the email column if the value is different
+    @abstractmethod
     def set_value_if_different(
         self, identification_value: str, email_value: str
     ) -> None:
@@ -27,10 +29,18 @@ class DataManipulation:
         raise NotImplementedError("Subclass must implement this method")
 
     # get data table as dataframe
+    @abstractmethod
     def get_dt_table(self) -> pd.DataFrame:
         """Get data table"""
 
-        return self.dt_table
+        raise NotImplementedError("Subclass must implement this method")
+
+    # identification column higienization
+    @abstractmethod
+    def identification_column_higienization(self) -> pd.DataFrame:
+        """Column higienization"""
+
+        raise NotImplementedError("Subclass must implement this method")
 
 
 # Data manipulation class for aposentados
@@ -39,18 +49,24 @@ class DataManipulationAposentados(DataManipulation):
 
     # init method
     def __init__(self, dt_table: pd.DataFrame) -> None:
-        super().__init__(dt_table)
+        super().__init__()
         self.column_identification_name = "CPF SERVIDOR"
+        self.dt_table = self.identification_column_higienization(dt_table)
+
+    def identification_column_higienization(self, dt_table) -> pd.DataFrame:
+        """Higienize the identification column"""
+
+        dt_table[self.column_identification_name] = dt_table[
+            self.column_identification_name
+        ].str.zfill(11)
+
+        return dt_table
 
     # get column list of identification values (cpf)
     def get_identification_column_list(self) -> pd.DataFrame:
         """Get column cpf servidor"""
 
-        dt_table_aposentados_column = self.dt_table[
-            self.column_identification_name
-        ].str.zfill(11)
-
-        return dt_table_aposentados_column.tolist()
+        return self.dt_table[self.column_identification_name].tolist()
 
     # set the value in the email column if the value is different
     def set_value_if_different(
@@ -83,6 +99,12 @@ class DataManipulationAposentados(DataManipulation):
                     self.column_email_name,
                 ] = email_value
 
+    # get data table as dataframe
+    def get_dt_table(self) -> pd.DataFrame:
+        """Get data table"""
+
+        return self.dt_table
+
 
 # Data manipulation class for pensionistas
 class DataManipulationPensionistas(DataManipulation):
@@ -90,18 +112,24 @@ class DataManipulationPensionistas(DataManipulation):
 
     # init method
     def __init__(self, dt_table: pd.DataFrame) -> None:
-        super().__init__(dt_table)
+        super().__init__()
         self.column_identification_name = "VÍNCULO PENSÃO (EDITADO)"
+        self.dt_table = self.identification_column_higienization(dt_table)
+
+    def identification_column_higienization(self, dt_table) -> pd.DataFrame:
+        """Higienize the identification column"""
+
+        dt_table[self.column_identification_name] = dt_table[
+            self.column_identification_name
+        ].str.slice(-8)
+
+        return dt_table
 
     # get column list of identification values (matricula)
     def get_identification_column_list(self) -> list[str]:
         """Get column matricula pensionista"""
 
-        dt_table_pensao_column = self.dt_table[
-            self.column_identification_name
-        ].str.slice(-8)
-
-        return dt_table_pensao_column.tolist()
+        return self.dt_table[self.column_identification_name].tolist()
 
     # set the value in the email column if the value is different
     def set_value_if_different(
@@ -113,15 +141,14 @@ class DataManipulationPensionistas(DataManipulation):
         if self.column_email_name not in self.dt_table.columns:
 
             self.dt_table.loc[
-                self.dt_table[self.column_identification_name].str.slice(-8)
-                == identification_value,
+                self.dt_table[self.column_identification_name] == identification_value,
                 self.column_email_name,
             ] = email_value
         else:
             # set the value in the email column if the value is different
             if (
                 self.dt_table.loc[
-                    self.dt_table[self.column_identification_name].str.slice(-8)
+                    self.dt_table[self.column_identification_name]
                     == identification_value,
                     self.column_email_name,
                 ].values[0]
@@ -129,7 +156,13 @@ class DataManipulationPensionistas(DataManipulation):
             ):
 
                 self.dt_table.loc[
-                    self.dt_table[self.column_identification_name].str.slice(-8)
+                    self.dt_table[self.column_identification_name]
                     == identification_value,
                     self.column_email_name,
                 ] = email_value
+
+    # get data table as dataframe
+    def get_dt_table(self) -> pd.DataFrame:
+        """Get data table"""
+
+        return self.dt_table
